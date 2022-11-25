4a5e9ea0ca848174185dba0f1f4dd67eed5ce864
Marius-McFly <marius-hofmann@gmx.de>
2022-11-25 12:30:48 +0100

    add navi as well

--- components/TheTopbar.vue
+++ components/TheTopbar.vue
@@ -1,42 +1,44 @@
 <template>
-<div class="topbar">
-    <div class="flex">
-        <p>I'm the topbar</p>
-        <a href="/">home</a>
-        <a href="/about">about</a>
-        <a href="/dynamic">dynamic</a>
+    <div class="topbar">
+        <div class="flex">
+            <p>I'm the topbar</p>
+            <a href="/">home</a>
+            <a href="/about">about</a>
+            <a href="/dynamic">dynamic</a>
+            <a v-for="item in lele" :href="item._path">{{ item._path }}</a>
+        </div>
+        
     </div>
-</div>
-</template>
-
-<script setup>
-const route = useRoute()
-</script>
-
-<style>
-.topbar {
-    background-color: grey;
-    text-align: center;
-    height: 80px;
-    width: 100%;
-    display: flex;
-    justify-items: center;
-    align-items: center;
-}
-
-p {
-    margin-right: 40px;
-    font-size: 32px;
-}
-
-.flex {
-    display: flex;
-    justify-items: center;
-    width: 100%;
-}
-
-a {
-    margin-right: 20px;
-    font-size: 24px;
-}
-</style>
+    </template>
+    
+    <script setup>
+    const route = useRoute()
+    const { data: lele } = await useAsyncData(route.name, () => queryContent('/').find())
+    </script>
+    
+    <style>
+    .topbar {
+        background-color: grey;
+        text-align: center;
+        width: 100%;
+        display: flex;
+        justify-items: center;
+        align-items: center;
+    }
+    
+    p {
+        margin-right: 40px;
+        font-size: 32px;
+    }
+    
+    .flex {
+        display: flex;
+        justify-items: center;
+        width: 100%;
+    }
+    
+    a {
+        margin-right: 20px;
+        font-size: 24px;
+    }
+    </style>
